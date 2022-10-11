e6a40f39168ab3fa9367d08963b7faa969f9c670
Ryush <ryush@idrsystems.co.kr>
2022-10-11 18:02:05 +0900

    react-blog commit

--- study/react-blog/src/App.js
+++ study/react-blog/src/App.js
@@ -1,23 +1,24 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let posts = '강남 고기 맛집';
+  let [글제목, 글제목변경] = useState('남자 코트 추천');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="black-nav" style={ {color:'yellow', fontSize: '30px'}}>
+        개발 Blog
+      </div>
+
+      <div className="list">
+        <h4>{ 글제목 }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+
     </div>
   );
 }
